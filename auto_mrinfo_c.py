09afee6b710b917572fc10f730dd1a05447534ec
yiqiaoxihui <937285794@qq.com>
2019-06-25 04:45:52 -0400

    add script

---
+++ auto_mrinfo_c.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import shlex
+import subprocess
+import time
+file_path=sys.argv[1]
+print  file_path
+
+fw_result=open(sys.argv[2],'w')
+fr=open(file_path,'r')
+while True:
+	ip=fr.readline()
+	if not ip:
+		break
+	else:
+		ip=ip.strip()
+		cmd="./mrinfo.out  "+ip
+		print cmd
+		cmd = shlex.split(cmd)
+		p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		while p.poll() is None:
+			line = p.stdout.readline()
+			fw_result.write(line)
+			print line.strip()
+fw_result.close()
